335d8d6ffc218066909f617978137c2421c2bc97
Dion Gatea <Dion.Gatea@hotmail.com>
2017-05-07 13:48:08 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  newQuote();
+  $(".fade").hide(0).delay(500).fadeIn(2000);
+    $(".newquote").click(function() {
+    outFade();
+    newQuote();
+    inFade();
+    });
+    $(".tweet").click(function(){
+    window.open("https://twitter.com/intent/tweet?text=" + string , "Twitter", "width=500,height=250");
+    });
+});
+
+var string;
+var author;
+var quote;
+
+function outFade() {
+  $(".quote, .author").fadeOut();
+}
+
+function inFade() {
+  $(".quote, .author").fadeIn();
+}
+
+function newQuote() {
+    var url = $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(data) {
+        author = data.author;
+        quote = data.quote;
+        string = quote + " - " + author;
+        $(".author").html("- " + author);
+        $(".quote").html('<h3>"' + quote + '"<h3>');
+    });
+}
